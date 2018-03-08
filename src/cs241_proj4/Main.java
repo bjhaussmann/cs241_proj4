a7ddebbd75783085c30a3356609104b2593cfda6
bjhaussmann <bjhaussmann@gmail.com>
2018-03-07 20:08:07 -0800

    added data files and created main

---
+++ src/cs241_proj4/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cs241_proj4;
+
+/**
+ * @author Ben
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
