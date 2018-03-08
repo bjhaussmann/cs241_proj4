1275c82f6f1b38868d6cc984ca8da8e12dd79bc7
bjhaussmann <bjhaussmann@gmail.com>
2018-03-07 21:48:51 -0800

    fixed type getNeighborIterator()

--- src/cs241_proj4/VertexInterface.java
+++ src/cs241_proj4/VertexInterface.java
@@ -16,7 +16,7 @@ public interface VertexInterface <T> {
 	public boolean isVisited();
 	public boolean connect(VertexInterface<T> endVertex, double edgeWeight);
 	public boolean connect (VertexInterface <T> endVertex);
-	public Iterator<VertexInterface<T>> getNeightboreIterator();
+	public Iterator<VertexInterface<T>> getNeightborIterator();
 	public Iterator<Double> getWeightIterator();
 	public boolean hasNeighbor ();
 	public VertexInterface<T> getUnvisitedNeighbor();
